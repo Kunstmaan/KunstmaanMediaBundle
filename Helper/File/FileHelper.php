c770f840faa3f148001503b5d0437d07e4968e0a
Umeku <andrejs.m@dzin.lv>
2015-02-24 17:39:10 +0200

    added an additional check to setFile for a case when file does not exist

--- Helper/File/FileHelper.php
+++ Helper/File/FileHelper.php
@@ -127,11 +127,13 @@ class FileHelper
     public function setFile(File $file)
     {
         $this->file = $file;
-        $this->media->setContent($file);
-        $this->media->setContentType($file->getMimeType());
-        $this->media->setUrl(
-            '/uploads/media/' . $this->media->getUuid() . '.' . $this->media->getContent()->getExtension()
-        );
+        if ($file->getPathname()) {
+            $this->media->setContent($file);
+            $this->media->setContentType($file->getMimeType());
+            $this->media->setUrl(
+                '/uploads/media/' . $this->media->getUuid() . '.' . $this->media->getContent()->getExtension()
+            );
+        }
     }
 
     /**
